4f27329858f1de60f844bb18b4efd7c5d3809522
Tom Parker <palfrey@tevp.net>
2017-03-05 14:32:39 +0000

    Make more readable debug by parsing Commands into an Enum

--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,11 @@ extern crate nom;
 #[macro_use]
 extern crate log;
 extern crate env_logger;
+#[macro_use]
+extern crate enum_primitive;
+extern crate num_traits;
+
+use num_traits::FromPrimitive;
 
 use std::fs::File;
 use std::io::Read;
@@ -25,7 +30,7 @@ fn array_to_vec(arr: &[u8]) -> Vec<u8> {
 named!(get_word<Words>,
     alt!(
         alpha  => { |word| Words::Word(array_to_vec(word)) } |
-        alt!(multispace | digit | is_a!(", !;:.()\"'-?"))  => { |_| Words::Junk }
+        alt!(multispace | digit | is_a!(", !;:.()\"'-?|&"))  => { |_| Words::Junk }
     )
 );
 
@@ -55,11 +60,9 @@ fn get_words(filename: &str) -> Result<Vec<String>, io::Error> {
                 match rest {
                     nom::IResult::Error(nom::verbose_errors::Err::Position(_, characters)) => {
                         let location = std::str::from_utf8(characters).map_err(io_str_error)?;
-                        println!("location {}", location.chars().take(50).collect::<String>());
-                    }
-                    _ =>{
-                        println!("rest {:?}", rest);
-                    }
+                        return Err(io::Error::new(io::ErrorKind::Other, format!("Don't know how to parse: {}", location.chars().take(50).collect::<String>())));
+                    },
+                    _ => {}
                 };
                 
                 //rest.to_result().map_err(io_str_error)?;
@@ -90,6 +93,34 @@ fn score(word: &str) -> u8 {
     return result;
 }
 
+enum_from_primitive! {
+    #[allow(non_camel_case_types)]
+    #[derive(Debug, PartialEq)]
+    enum Command {
+        PUSH = 5,
+        DISCARD = 6,
+        ADD = 7,
+        INPUT = 8,
+        OUTPUT = 9,
+        SUBTRACT = 10,
+        SWAP = 11,
+        DUP = 12,
+        SKIP_AHEAD_ZERO = 13,
+        SKIP_AHEAD_NONZERO = 14,
+        SKIP_BACK_ZERO = 15,
+        SKIP_BACK_NONZERO = 16,
+        STOP = 17,
+        NOP,
+    }
+}
+
+fn action(act: u8) -> Command {
+    return match Command::from_u8(act) {
+        Some(val) => val,
+        None => Command::NOP
+    }
+}
+
 fn main() {
     env_logger::init().unwrap();
     let mut stack: Vec<u8> = Vec::new();
@@ -97,32 +128,68 @@ fn main() {
     let scores: Vec<u8> = words.iter().map(|w| score(&w)).collect();
     let mut pc: usize = 0;
     loop {
-        debug!("Running command {}", scores[pc]);
-        match scores[pc] {
-            0 => {},
-            5 => {
-                debug!("Pushing {} to stack", scores[pc+1]);
-                stack.push(scores[pc+1]);
+        debug!("'{}' = {} ({:?})", words[pc], scores[pc], action(scores[pc]));
+        match action(scores[pc]) {
+            Command::PUSH => {
                 pc +=1;
+                debug!("Pushing {} to stack", scores[pc]);
+                stack.push(scores[pc]);
             }
-            7 => {
+            Command::DISCARD => {stack.pop().expect("stack value");},
+            Command::ADD => {
                 let x = stack.pop().expect("first value");
                 let y = stack.pop().expect("second value");
-                stack.push(x+y);
+                stack.push(x.wrapping_add(y));
             }
-            8 => {
+            Command::INPUT => {
                 // FIXME: read from stdin
                 stack.push('a' as u8);
             }
-            9 => print!("{}", stack.pop().expect("character on stack") as char),
-            15 => {
+            Command::OUTPUT => print!("{}", stack.pop().expect("character on stack") as char),
+            Command::SUBTRACT => {
+                let x = stack.pop().expect("first value");
+                let y = stack.pop().expect("second value");
+                stack.push(y.wrapping_sub(x));
+            }
+            Command::SWAP => {
+                let x = stack.pop().expect("first value");
+                let y = stack.pop().expect("second value");
+                stack.push(x);
+                stack.push(y);
+            }
+            Command::DUP => {
+                let x = stack.pop().expect("first value");
+                stack.push(x);
+                stack.push(x);
+            }
+            Command::SKIP_AHEAD_NONZERO => {
+                let check = stack.pop().expect("check value");
+                pc +=1;
+                if check != 0 {
+                    pc += scores[pc] as usize;
+                }
+            }
+            Command::SKIP_AHEAD_ZERO => {
+                let check = stack.pop().expect("check value");
+                pc +=1;
+                if check == 0 {
+                    pc += scores[pc] as usize;
+                }
+            }
+            Command::SKIP_BACK_ZERO => {
                 let check = stack.pop().expect("check value");
                 if check == 0 {
                     pc -= scores[pc+1] as usize;
                 }
             }
-            17 => break,
-            other => println!("Don't know command {}", other)
+            Command::SKIP_BACK_NONZERO => {
+                let check = stack.pop().expect("check value");
+                if check != 0 {
+                    pc -= scores[pc+1] as usize;
+                }
+            }
+            Command::STOP => break,
+            Command::NOP => {}
         }
         pc += 1;
         if pc == scores.len() {
